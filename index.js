94863e5d55e5eaccf973f2093d82cb1972ca2789
sabrinara <you@example.com>
2023-11-08 23:39:36 +0600

    update booking

--- index.js
+++ index.js
@@ -83,6 +83,13 @@ async function run() {
       res.send(bookings);
     })
 
+    // delete booking
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingsCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
